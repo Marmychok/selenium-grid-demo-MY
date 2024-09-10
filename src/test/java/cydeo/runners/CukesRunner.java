ae85c401d9a2a51fb364431860458c317e198cf2
Marmychok <161072017+Marmychok@users.noreply.github.com>
2024-09-11 01:12:53 +0600

    with

--- src/test/java/cydeo/runners/CukesRunner.java
+++ src/test/java/cydeo/runners/CukesRunner.java
@@ -9,10 +9,13 @@ import org.junit.runner.RunWith;
         plugin = {
                 "json:target/cucumber.json" ,
                 "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
                 },
         features = "src/test/resources/features",
-        glue = "cydeo/steps",
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,
         publish = true
+        //tags="@scenariOutline"
 )
 public class CukesRunner {}
